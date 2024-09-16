d486f94a5875886bb4e567667de9093e5b65d906
Soubhik <soubhikpramanik10@gmail.com>
2024-09-17 00:06:51 +0530

    Second command

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const [a,b] = useState(0);
   return (
     <div className='w-full h-screen  bg-black text-white flex justify-center items-center'>
       <div>
-          <h1 className='text-green-500 text-2xl text-center'>{a}</h1>
+          <h1 className='text-green-500 text-2xl text-center mb-4'>{a}</h1>
 
             <button className='border-solid border-purple-600 bg-rose-500  p-2 rounded-md mr-2 '
             onClick={()=>b(a+1)}
