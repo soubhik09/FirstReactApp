f378ada86b2c82f5316f04b89a4d09f9ae00d6a7
Soubhik <soubhikpramanik10@gmail.com>
2024-09-16 23:59:15 +0530

    This My First React App

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import react, { useState } from 'react'
+
+function App() {
+const [a,b] = useState(0);
+  return (
+    <div className='w-full h-screen  bg-black text-white flex justify-center items-center'>
+      <div>
+          <h1 className='text-green-500 text-2xl text-center'>{a}</h1>
+
+            <button className='border-solid border-purple-600 bg-rose-500  p-2 rounded-md mr-2 '
+            onClick={()=>b(a+1)}
+            >Plus</button>
+            <button className='border-solid border-purple-600 bg-rose-500  p-2 rounded-md'
+            onClick={()=>b(a-1)}
+            >Minus</button>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default App
